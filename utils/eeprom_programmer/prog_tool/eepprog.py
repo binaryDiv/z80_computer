56f69fca3234ab57585801b7d68e86984286d18b
binaryDiv <binarydiv@gmail.com>
2018-08-24 22:42:01 +0200

    Argument parsing, added usage()

--- utils/eeprom_programmer/prog_tool/eepprog.py
+++ utils/eeprom_programmer/prog_tool/eepprog.py
@@ -4,19 +4,69 @@ import sys
 import getopt
 # import serial
 
+# Global variables for program parameters
+verbose = False
+serial_device = "/dev/ttyUSB0"
+serial_baudrate = 9600
+
+
+def usage():
+    """Prints help text."""
+
+    print(f"""
+Usage: {sys.argv[0]} [options]
+
+Options:
+  -h, --help                      show this help message and exit
+  -v, --verbose                   be verbose
+  -d DEVICE, --device=DEVICE      use DEVICE as serial device
+                                  (default: /dev/ttyUSB0)
+"""[1:-1])
+
 
 def parse_args():
     """Parse command line arguments and set global variables."""
 
-    optlist, args = getopt.gnu_getopt(sys.argv[1:], 'abc:d:', '--verbose')
+    # Global variables for program options (yes, yes, I know...)
+    global verbose, serial_device, serial_baudrate
 
-    print("optlist: " + str(optlist))
-    print("args: " + str(args))
+    try:
+        # Try to parse argument strings
+        optlist, args = getopt.gnu_getopt(
+            sys.argv[1:],
+            "hvd:",
+            ["help", "verbose", "device"]
+        )
+    except getopt.GetoptError as err:
+        # Invalid option, print help and exit
+        print(err, "\n")
+        usage()
+        sys.exit(2)
 
-    # TODO parse arguments...
+    # Parse all the arguments
+    for opt, val in optlist:
+        if opt == "-h" or opt == "--help":
+            # Print help
+            usage()
+            sys.exit()
+
+        elif opt == "-v" or opt == "--verbose":
+            # Verbose
+            print("verbose!")
+            verbose = True
+
+        elif opt == "-d" or opt == "--device":
+            # Set device filename
+            print("device: " + val)
+            serial_device = val
+
+        else:
+            assert False, "unhandled option"
 
 
 def setup_serial():
+    """Setup serial device."""
+
     # TODO ...
     pass
 
@@ -24,8 +74,7 @@ def setup_serial():
 def main():
     """Main function. Does the thing."""
 
-    # TODO testing...
-    print("Meow :3")
+    # Parse program arguments
     parse_args()
 
 
